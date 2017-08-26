7ee353132537ffeedd87d1df08229003a6e21583
ryanmavrinac28 <ryanmavrinac28@gmail.com>
2017-08-25 19:15:17 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,144 @@
+
+<!DOCTYPE html>
+<!----- GET THE FUCK OUT OF MY SOURCE FAGGOT ----->
+<!-----
+....................../´¯/)......................
+....................,/¯../......................
+.................../..../......................
+............./´¯/'...'/´¯¯`·¸......................
+........../'/.../..../......./¨¯\......................
+........('(...´...´.... ¯~/'...')......................
+.........\.................'...../......................
+..........''...\.......... _.·´......................
+............\..............(......................
+..............\.............\.........................
+----->
+<!----- GET THE FUCK OUT OF MY SOURCE FAGGOT ----->
+<html >
+<head>
+<meta charset="UTF-8">
+<title>Hall Of Skids</title>
+</head>
+<style>
+body {
+background: #222;
+color: #435160;
+font-family: "Open Sans", sans-serif;
+}
+h1{
+font-size:46px;
+color: red;
+text-transform:uppercase;
+text-shadow: 0 0 10px red;
+}
+h2 {
+font-size:25px;
+color: #fff;
+text-transform:uppercase;
+}
+
+figcaption:before,
+figcaption:after {
+background-color: red;
+text-shadow: 0 0 10px red;
+width: 100%;
+height: 4px;
+content: "";
+display: block;
+-webkit-transition: all 0.35s ease-in-out;
+transition: all 0.35s ease-in-out;
+}
+
+#content.full-page .inside-block {
+display: block;
+margin: 0 auto;
+text-align: center;
+width: 360px;
+background-color: rgba(0, 0, 0, 0.3);
+height: 100%;
+-webkit-box-sizing: content-box;
+-moz-box-sizing: content-box;
+box-sizing: content-box;
+padding: 50px;
+color: white;
+overflow: auto;
+}
+#content.full-page {
+position: fixed;
+width: 100%;
+height: 100%;
+padding: 0 0 45px 0 !important;
+top: -45px;
+-moz-box-sizing: content-box;
+box-sizing: content-box;
+}
+h3 {
+width: 600px;
+color: #fff;
+font-size: 15px;
+font-weight: bold;
+font-size: 1em;
+text-align: center;
+margin-bottom: 20px;
+margin-left: -130px;
+}
+h4 {
+width: 600px;
+color: #fff;
+font-size: 15px;
+font-weight: bold;
+font-size: 1em;
+text-align: center;
+margin-bottom: 20px;
+margin-left: -130px;
+}
+h5 {
+width: 600px;
+color: red;
+font-size: 15px;
+font-weight: bold;
+font-size: 1em;
+text-align: center;
+margin-left: -130px;
+}
+a {
+color: #435160;
+text-decoration: none;
+}
+audio {
+background-color: rgba(0, 0, 0, 0.3);
+}
+</style>
+<body>
+<div id="content" class="col-md-12 full-page">
+<div class="inside-block" <div style="background-color:rgba(0, 0, 0, 0.3);">
+</br>
+<figcaption>
+<h1>
+Hall Of Skids
+</h1>
+</figcaption>
+</br></br>
+<h2>
+@WereTheGoons
+</br></br>
+@LizardLands
+</br></br>
+@LulzSec
+</br></br>
+@FinestSquad
+</br></br>
+@_CyberTeam_
+</br></br>
+@InquisionSquad
+</br></br>
+@PoodleCorp
+</h2></br>
+</center>
+<center>
+<audio loop controls autoplay>
+<source src="UrHarmless.mp3">
+</center>
+</audio>
+</body>
+</html>
